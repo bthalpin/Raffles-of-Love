542b00cf08b1f52672c368c84630ed50ff4ce568
jsnyder159 <jw.snyder159@gmail.com>
2022-04-23 02:17:14 -0400

    added index model

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Ticket = require('./Ticket');
+const Product = require('./Product');
+const Charity = require('./Charity');
+
+module.exports = { User, Ticket, Product, Charity };
