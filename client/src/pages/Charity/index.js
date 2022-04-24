8465707da4a77f22fae23b95945fc27632d66ef7
Brian Halpin <brian.t.halpin@gmail.com>
2022-04-23 23:37:38 -0400

    Add basic blurb format for main page

--- client/src/pages/Charity/index.js
+++ client/src/pages/Charity/index.js
@@ -36,6 +36,9 @@ function Charity () {
     ]
     return (
             <Container className="d-flex flex-wrap justify-content-around m-auto">
+                <p className="my-5 p-2">
+                <span className="display-1">Welcome</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
                 {tempCharities.map((charity,index)=>{
                     return (
                         <Card className="charityCard m-2 p-3 text-center" key={index}>
