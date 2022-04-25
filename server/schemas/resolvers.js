c2e2b8fffb02f586c22bb7a9dcc3f4e66da83433
Dan Kim <danchanyoungkim@gmail.com>
2022-04-25 10:00:17 -0400

    Added some mutations into resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,11 +12,23 @@ const resolvers = {
     charity: async (parent, { _id }) => {
       return Charity.findOne({ _id: _id });
     },
-    products: {
-      
-    },
-    product: {
+    products: async (parent, { charity, name }) => {
+      const params = {};
 
+      if (charity) {
+        params.charity = charity;
+      }
+
+      if (name) {
+        params.name = {
+          $regex: name
+        };
+      }
+
+      return await Product.find(params).populate('charity');
+    },
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id).populate('charity');
     },
     user: async (parent, args, context) => {                              // Context will retrieve the logged-in user without specifically searching for them.
       if (context.user) {
@@ -36,6 +48,36 @@ const resolvers = {
     checkout: {
 
     },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    updateUser,
+    updateProduct,
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
   }
 }
 
+
+
+module.exports = resolvers;
