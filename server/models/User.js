bfc861b76380c91a75f1c1e957f4e0d817ad98a3
jsnyder159 <jw.snyder159@gmail.com>
2022-04-25 10:57:23 -0400

    fixed spelling errors on seeds, added ticket array to users model

--- server/models/User.js
+++ server/models/User.js
@@ -22,13 +22,16 @@ const userSchema = new Schema({
         required: true,
         minlength: 5
     },
-    // charityAdmin: [{
-    //     type: Boolean,
-    //     default: false,
-    //     charity: [Charity.schema]},
-    // ],
+    charityAdmin: [
+        // linked to charity ID
+        // if not admin null otherwise would be linked to charity ID
+        // type: Boolean,
+        // default: false,
+        // charity: [Charity.schema]},
+    ],
     tickets: [Ticket.schema],
     // favCharitys: [Charity.schema],
+    // street addres // city // state
 });
 
 
---
+++ server/models/charityStatus.js
@@ -0,0 +1,2 @@
+// for charity status model ice box - form that emails information for someone to confirm
+// charity status and legitimacy.
