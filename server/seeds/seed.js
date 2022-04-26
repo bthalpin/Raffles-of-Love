34ea7feb28fba7091bbf8083792a23949145bbb7
Dan Kim <danchanyoungkim@gmail.com>
2022-04-25 21:26:48 -0400

    finished draft for seeds.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,2 +1,15 @@
 const db = require('../config/connection');
 const { User, Ticket, Product, Charity } = require('../models');
+const productSeeds = require('./productSeeds.json');
+
+db.once('open', async () => {
+    try {
+      await Product.deleteMany({});
+      await Product.create(productSeeds);
+  
+      console.log('all done!');
+      process.exit(0);
+    } catch (err) {
+      throw err;
+    }
+  });
