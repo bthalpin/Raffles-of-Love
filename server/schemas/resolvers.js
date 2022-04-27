ca926b2c5dad31f60a99e1242ba273aab24d79a9
jsnyder159 <jw.snyder159@gmail.com>
2022-04-27 00:37:44 -0400

    added checkout back changed vars commented out images

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -46,42 +46,42 @@ const resolvers = {
       return await Ticket.findById(_id).populate('product');                      // Using the parameter to find the matching ticket in the collection.
     },
 
-    // checkout: async (parent, args, context) => {
-    //     const url = new URL(context.headers.referer).origin;
-    //     const ticket = new Ticket({ products: args.products });
-    //     const line_items = [];
-  
-    //     const { products } = await ticket.populate('products');
-  
-    //     for (let i = 0; i < products.length; i++) {
-    //       const product = await stripe.products.create({
-    //         name: products[i].name,
-    //         description: products[i].description,
-    //         images: [`${url}/images/${products[i].image}`]
-    //       });
-  
-    //       const price = await stripe.prices.create({
-    //         product: product.id,
-    //         unit_amount: products[i].price * 100,
-    //         currency: 'usd',
-    //       });
-  
-    //       line_items.push({
-    //         price: price.id,
-    //         quantity: 1
-    //       });
-    //     }
-  
-    //     const session = await stripe.checkout.sessions.create({
-    //       payment_method_types: ['card'],
-    //       line_items,
-    //       mode: 'payment',
-    //       success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //       cancel_url: `${url}/`
-    //     });
-  
-    //     return { session: session.id };
-    //   }
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const cart = [];
+
+      const { products } = await order.populate('products');
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description
+          // images: [`${url}/images/${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        cart.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        cart,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
   },
   // Define the functions that will fulfill the mutations.
   Mutation: {
