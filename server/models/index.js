f04f083e6b3f6a4b9ef811c6af4e2339cb0d6cd7
jsnyder159 <jw.snyder159@gmail.com>
2022-04-27 13:56:28 -0400

    updated order exports

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const User = require('./User');
 const Ticket = require('./Ticket');
 const Product = require('./Product');
 const Charity = require('./Charity');
+const Order = require('./Order');
 
-module.exports = { User, Ticket, Product, Charity };
+module.exports = { User, Ticket, Product, Charity, Order };
