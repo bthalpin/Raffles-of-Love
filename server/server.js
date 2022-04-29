913d5fcb9a5095863907734764d161bf977bfa37
Brian Halpin <brian.t.halpin@gmail.com>
2022-04-29 15:04:35 -0400

    test

--- server/server.js
+++ server/server.js
@@ -26,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
