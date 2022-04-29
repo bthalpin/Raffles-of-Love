ff96c3ba795c003baf66dccf2668928a77328d7b
Dan Kim <danchanyoungkim@gmail.com>
2022-04-29 04:10:12 -0400

    Finished up touches

--- client/src/pages/SingleCharity/index.js
+++ client/src/pages/SingleCharity/index.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import {ProductCard} from '../../components/';
 import {Card,Container} from 'react-bootstrap';
-// import { tempProductData } from '../../tempProductData';
 import { useStoreContext } from "../../utils/GlobalState";
 import {SINGLE_CHARITY,PRODUCTS_BY_CHARITY} from '../../utils/queries';
 import { UPDATE_CURRENT_CHARITY,UPDATE_CHARITY_PRODUCTS } from '../../utils/actions';
@@ -12,7 +11,6 @@ import './single.css';
 function Charity () {
     const {charityId} = useParams()
     const [state, dispatch] = useStoreContext();
-    const tempProductData = state.products
     console.log(charityId)
     const { loading, data } = useQuery(SINGLE_CHARITY,{
         variables:{charityId:charityId},
@@ -40,12 +38,6 @@ function Charity () {
         }
     },[results])
     console.log(state.currentCharity,state.charityProducts)
-    const tempCharity = 
-        {
-            name:"Temp Name",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with ",
-            image:'/logo192.png'
-        }
     
     return (
         <div className="singleCharityPage ">
@@ -56,7 +48,7 @@ function Charity () {
                         
                         <Card className="singleCharityCard m-2 p-3 text-center col-lg">
                             <Card.Title className="title m-2">{state.currentCharity.name}</Card.Title>
-                            <img className="singleCharityImage" src={state.currentCharity.image}/>
+                            <img className="singleCharityImage" src={state.currentCharity.image} alt=""/>
                             <Card.Body>
                                 <p>Their Mission: </p>{state.currentCharity.mission}
                             </Card.Body>
@@ -66,7 +58,7 @@ function Charity () {
                         </Card>
                         
                         <Card className="singleCharityCard m-2 p-3 col-lg">
-                            <iframe width="420" height="315" src={state.currentCharity.youtube} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+                            <iframe width="420" height="315" src={state.currentCharity.youtube} frameborder="0" allowfullscreen=""></iframe>
                         </Card>
                        
             </Container>
