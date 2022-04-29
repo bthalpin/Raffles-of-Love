8c2c16e3d62de03871ff2b390f7a7f0e6b1b0e40
jsnyder159 <jw.snyder159@gmail.com>
2022-04-29 08:17:16 -0400

    added ticket styles

--- client/src/components/RaffleTicket/index.js
+++ client/src/components/RaffleTicket/index.js
@@ -58,7 +58,7 @@ function RaffleTicket ({tickets}) {
                     return(
                         <Link className="ticketLink m-2" to={`/Product/${ticket.product._id}`} key={index}>
                             <Card className={checkWinner(ticket)}>
-                                <Card.Header>
+                                <Card.Header className="cardTitle">
                                     <Card.Title className="text-center">Ticket #{ticket.ticketNumber}</Card.Title>
                                 </Card.Header>
                                 <Card.Body className="">
