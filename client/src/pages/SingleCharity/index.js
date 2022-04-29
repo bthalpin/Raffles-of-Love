e26f693732b66518f64a174457f259dfe6595c88
Dan Kim <danchanyoungkim@gmail.com>
2022-04-29 02:10:41 -0400

    finished seeding of youtube

--- client/src/pages/SingleCharity/index.js
+++ client/src/pages/SingleCharity/index.js
@@ -66,7 +66,7 @@ function Charity () {
                         </Card>
                         
                         <Card className="singleCharityCard m-2 p-3 col-lg">
-                            <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" allowfullscreen=""></iframe>
+                            <iframe width="420" height="315" src={state.currentCharity.youtube} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
                         </Card>
                        
             </Container>
